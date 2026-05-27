6e1d56ff3e8bbf6378a1e56c8a5a6deef7a173f1
AlejandroXieHu <a01743620@tec.mx>
2026-05-26 21:13:21 -0600

    ALUControl.v

---
+++ ALUControl.v
@@ -0,0 +1,35 @@
+module ALUControl (
+
+    input [1:0] ALUOp,
+    input [2:0] funct3,
+    input op,
+    input funct7_5,
+    output reg [2:0] ALUControl
+
+);
+
+    always @(*)
+        begin
+            if (ALUOp == 2'b00)
+                ALUControl = 3'b000;
+            else if (ALUOp == 2'b01)
+                ALUControl = 3'b001;
+            else
+                begin
+                    case (funct3)
+                        3'b000:
+                            begin
+                                if ({op, funct7_5} == 2'b11)
+                                    ALUControl = 3'b001;
+                                else
+                                    ALUControl = 3'b000;
+                            end
+                        3'b010:ALUControl = 3'b101;
+                        3'b011:ALUControl = 3'b011;
+                        3'b111:ALUControl = 3'b010;
+                        default:ALUControl = 3'b000;
+                    endcase
+                end
+        end
+
+endmodule
