60fb582522670b9bf29648af14710388b63f41e1
AlejandroXieHu <a01743620@tec.mx>
2026-05-29 15:16:30 -0600

    ALU_Control.v

--- ALU_Control.v
+++ ALU_Control.v
@@ -1,4 +1,4 @@
-module ALU_Control (
+module ALU_Decoder (
 
     input [1:0] ALUOp,
     input [2:0] funct3,
@@ -11,23 +11,23 @@ module ALU_Control (
     always @(*)
         begin
             if (ALUOp == 2'b00)
-                ALUControl = 3'b000;
+                ALUControl = 3'b000; // Suma por defecto (para lw, sw)
             else if (ALUOp == 2'b01)
-                ALUControl = 3'b001;
+                ALUControl = 3'b001; // Resta por defecto (para beq)
             else
                 begin
                     case (funct3)
                         3'b000:
                             begin
                                 if ({op, funct7_5} == 2'b11)
-                                    ALUControl = 3'b001;
+                                    ALUControl = 3'b001; // Operación: Resta (sub)
                                 else
-                                    ALUControl = 3'b000;
+                                    ALUControl = 3'b000; // Operación: Suma (add / addi)
                             end
-                        3'b010:ALUControl = 3'b101;
-                        3'b011:ALUControl = 3'b011;
-                        3'b111:ALUControl = 3'b010;
-                        default:ALUControl = 3'b000;
+                        3'b010:ALUControl = 3'b101; // Operación: SLT (Set Less Than)
+                        3'b011:ALUControl = 3'b011; // Operación: OR lógico
+                        3'b111:ALUControl = 3'b010; // Operación: AND lógico
+                        default:ALUControl = 3'b000; // Operación por defecto: Suma
                     endcase
                 end
         end
