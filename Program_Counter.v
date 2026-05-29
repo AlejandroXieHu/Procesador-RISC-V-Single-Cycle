3d11876d34c5e5a8a7997086ddbd054e09894ad1
AlejandroXieHu <a01743620@tec.mx>
2026-05-28 21:56:25 -0600

    Program_Counter.v

--- Program_Counter.v
+++ Program_Counter.v
@@ -3,16 +3,20 @@ module Program_Counter (
     input clk,
     input rst,
     input [31:0] pc_next,
-    output reg [31:2] pc
+    output reg [31:0] pc
 
 );
+
+    reg [31:2] pc_reg;
 
     always @(posedge clk or posedge rst)
         begin
             if (rst)
-                pc <= 32'b0;
+                pc_reg <= 30'b0;
             else
-                pc <= pc_next;
+                pc_reg <= pc_next[31:2];
         end
+
+    assign pc = {pc_reg, 2'b00};
 
 endmodule
