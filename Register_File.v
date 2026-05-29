6eb2068eab513363bcb539d0f99247dbf69762ea
AlejandroXieHu <a01743620@tec.mx>
2026-05-29 15:18:12 -0600

    Register_File.v
    Refactor register read and write logic to handle zero address case.

--- Register_File.v
+++ Register_File.v
@@ -13,16 +13,33 @@ module Register_File (
 
     reg [31:0] REG [31:0];
 
-    always @(posedge clk)
-        begin
-            RD1 = REG[A1];
-            RD2 = REG[A2];
-        end
-
     always @(*)
         begin
-            if (WE3 == 1)
-                REG[A3] = WD3;
+            if (A1 == 5'b0)
+                begin
+                    RD1 = 32'b0;
+                end
+            else
+                begin
+                    RD1 = REG[A1];
+                end
+
+            if (A2 == 5'b0)
+                begin
+                    RD2 = 32'b0;
+                end
+            else
+                begin
+                    RD2 = REG[A2];
+                end
+        end
+
+    always @(posedge clk)
+        begin
+            if (WE3 == 1'b1 && A3 != 5'b0)
+                begin
+                    REG[A3] <= WD3;
+                end
         end
 
 endmodule
