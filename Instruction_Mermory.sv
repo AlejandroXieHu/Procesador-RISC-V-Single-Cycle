18bb997f442efb47071209530cc717533fc5a474
AlejandroXieHu <a01743620@tec.mx>
2026-05-29 15:17:10 -0600

    Instruction_Memory.sv

--- Instruction_Mermory.sv
+++ Instruction_Mermory.sv
@@ -1,21 +1,17 @@
 module Instruction_Memory (
 
-	input clk,
 	input [31:0] A,
-	output reg [31:0] RD
+	output [31:0] RD
 
 );
 
-	reg [31:0] instr_mem [0:2]; //0:10_000
+	reg [31:0] instr_mem [0:31]; //0:10_000
 	
 	initial 
 		begin
 			$readmemh("instrMem.hex", instr_mem);
 		end
 	
-	always @(posedge clk)
-		begin
-			RD <= instr_mem[A[31:2]];
-		end
+	assign RD = instr_mem[A[31:2]];
 
 endmodule
